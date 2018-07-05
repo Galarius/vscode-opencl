0afeed4c120cbf6bed7732a61d2353760e92dc49
Ilya Shoshin <galarius.is@gmail.com>
2018-07-05 22:10:26 +0300

    Check astyle availability.

--- src/formattingProvider.ts
+++ src/formattingProvider.ts
@@ -3,17 +3,33 @@
 import * as vscode from 'vscode'; 
 import * as cmd from './cmd';
 
+function isAvailable(): Promise<boolean> {
+    let command = cmd.buildCommand(['astyler', '--version'])
+    return cmd.execute(command).then(function(output) {
+        let version = output.toString()
+        return version.startsWith('Artistic Style Version')
+    }, (error: any) => {
+        console.error(error);
+        vscode.window.showErrorMessage(error);
+        return false
+    });
+}
+
 export class OpenCLDocumentFormattingEditProvider implements vscode.DocumentFormattingEditProvider {
     
-    public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.TextEdit[] {
-        let filename = document.fileName;
-
-        let command = cmd.buildCommand(['astyle', "-n", filename])
-        cmd.execute(command).then(function(output){
+    public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.TextEdit[] {        
+        
+        isAvailable().then(function(output) {
+            let filename = document.fileName
+            let command = cmd.buildCommand(['astyle', '-n', filename])
+            cmd.execute(command)
+        }, (error: any) => {
+            let err = "Artistic Style is not available at PATH!";
+            console.error(err);
+            vscode.window.showErrorMessage("Error: " + err);
         });
 
-        let result: vscode.TextEdit[] = [];
-        return result;
+        let result: vscode.TextEdit[] = []
+        return result
     }
-
 }		
